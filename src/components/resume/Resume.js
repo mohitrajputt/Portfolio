542b217a853ad59d2a48e5ed542c54dfacde8b1d
Mohit Rajput <mohit.rajput@matpatra.com>
2025-08-24 19:21:45 +0530

    update resume PDF to latest version and format code

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,26 +1,31 @@
 import styles from "./Resume.module.css";
-import resumePdf from "../../assets/Mohit_resume_sde.pdf";
+import resumePdf from "../../assets/Mohit_Rajput_24082025.pdf";
 import resumePng from "../../assets/resume-511.png";
 
 function Resume() {
-    return (
-        <section className={styles.resumeSection} >
-            <div className={styles.resumeContainerD} >
-                <div className={styles.resume} >
-                    <embed src={resumePdf} type="application/pdf" />
-                </div>
-            </div>
-            <div className={styles.resumeContainerM} >
-                <img src={resumePng} />
-                <div className={styles.downloadButton}  >
-                    <a className={styles.downloadButton} href={resumePdf} download="Resume-Mohit.pdf" rel="noopener noreferrer" >
-                        <i className="fa-solid fa-download"></i>
-                        <span>Download Resume</span>
-                    </a>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className={styles.resumeSection}>
+      <div className={styles.resumeContainerD}>
+        <div className={styles.resume}>
+          <embed src={resumePdf} type="application/pdf" />
+        </div>
+      </div>
+      <div className={styles.resumeContainerM}>
+        <img src={resumePng} />
+        <div className={styles.downloadButton}>
+          <a
+            className={styles.downloadButton}
+            href={resumePdf}
+            download="Resume-Mohit.pdf"
+            rel="noopener noreferrer"
+          >
+            <i className="fa-solid fa-download"></i>
+            <span>Download Resume</span>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Resume;
